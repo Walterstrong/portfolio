6e482a3a2bf093cf1e1fc0567672e63c39639813
Tojimatov Abdukarim <tojimatovabdukarim@gmail.com>
2023-04-22 02:21:26 +0900

    fix: fixed

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# PRODUCTION
+git checkout master
+git reset --hard
+git pull origin master
+
+npm i yarn -g
+yarn global add serve
+yarn 
+yarn run build
+pm2 start "yarn run start:prod" --name=PORTFOLIO
+
+# DEVELOPMENT
+# npm i yarn -g
+# yarn 
+# pm2 start "yarn run start" --name=YOURSHOES-REACT
